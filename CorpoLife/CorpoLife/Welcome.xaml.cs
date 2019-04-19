4d4e3762a31e62aadec4ae6e8b2a7cea64a007e2
Klaudia Staszkiewicz <staszkiewicz.km@gmail.com>
2019-04-19 23:33:37 +0200

    welcome

--- CorpoLife/CorpoLife/Welcome.xaml.cs
+++ CorpoLife/CorpoLife/Welcome.xaml.cs
@@ -77,55 +77,6 @@ namespace CorpoLife
         //
         private void MainGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            var dialog = new LoginPrompt();
-            var client = GlobalUsage.GetRtClient();
-            dialog.Closing += (_sender, _e) =>
-            {
-
-                var d = _sender as LoginPrompt;
-                if (!(d.Canceled))
-                {
-
-                    var id = Convert.ToInt32(d.InputTextBox.Text);
-                    var password = d.PasswrdInput.Password;
-
-                    /*var response = client.LogIn(new LoginRequest {Id = id, Password = password});
-                    if (response.State)
-                    {
-                        GlobalUsage.CurrentUser.level = response.Level;
-                        GlobalUsage.CurrentUser.workerID = id;
-                        GlobalUsage.CurrentUser.coffee = false;
-                        GlobalUsage.CurrentUser.emergency = false;
-                        GlobalUsage.CurrentUser.teamName = response.Team;
-                        GlobalUsage.CurrentUser.name = response.Name;
-                        GlobalUsage.CurrentUser.teamID = response.TeamId;
-                        */
-                    GlobalUsage.CurrentUser.level = 2;
-                    switch (GlobalUsage.CurrentUser.level)
-                        {
-                            case 1:
-                                var workerWelcome = new WorkerView();
-                                workerWelcome.Show();
-                                break;
-                            case 2:
-                                var leaderWelcome = new WorkerView();
-                                leaderWelcome.Show();
-                                break;
-                            case 3:
-                                var headWelcome = new HeadView();
-                                headWelcome.Show();
-                                break;
-                            case 4:
-                                var adminWelcome = new HeadView();
-                                adminWelcome.Show();
-                                break;
-                        }
-
-                        Close();
-                    //}
-                }
-            };
-            dialog.Show();
         }
     }
 }
