9c67feffb5d2e836263da4fac90ca5c9df6cd565
Klaudia Staszkiewicz <staszkiewicz.km@gmail.com>
2019-04-19 23:08:23 +0200

    no message

--- CorpoLife/CorpoLife/Welcome.xaml.cs
+++ CorpoLife/CorpoLife/Welcome.xaml.cs
@@ -15,48 +15,80 @@ namespace CorpoLife
             InitializeComponent();
         }
 
-        
-        
-       // private void LogIn_Click(object sender, RoutedEventArgs e)
-       // {
-       //     int level = GlobalUsage.CurrentUser.level;
-       //
-       //     switch (level)
-       //     {
-       //         case 1:
-       //             WorkerView workerwelcome = new WorkerView();
-       //             workerwelcome.Show();
-       //             break;
-       //         case 2:
-       //             WorkerView leaderwelcome = new WorkerView();
-       //             leaderwelcome.Show();
-       //             break;
-       //         case 3:
-       //             HeadView headwelcome = new HeadView();
-       //             headwelcome.Show();
-       //             break;
-       //         case 4:
-       //             HeadView adminwelcome = new HeadView();
-       //             adminwelcome.Show();
-       //             break;
-       //
-       //     }
-       //     this.Close();
-       //
-       // }
-       //
+        public string PasswordText
+        {
+            get => PasswrdInput.Password;
+            set => PasswrdInput.Password = value;
+        }
+
+        public string IdText
+        {
+            get => InputTextBox.Text;
+            set => InputTextBox.Text = value;
+        }
+
+        public bool Canceled { get; set; }
+
+        private void BtnCancel_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Canceled = true;
+            Close();
+        }
+
+        private void BtnOk_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //TODO response.State instead of true
+            //var response = GlobalUsage.GetRtClient().LogIn(new MessagesPack.LoginRequest { Id = Convert.ToInt32(IdText), Password = PasswordText });
+            //response.State
+            if (true)
+            {
+                Canceled = false;
+                Close();
+            }
+        }
+
+        // private void LogIn_Click(object sender, RoutedEventArgs e)
+        // {
+        //     int level = GlobalUsage.CurrentUser.level;
+        //
+        //     switch (level)
+        //     {
+        //         case 1:
+        //             WorkerView workerwelcome = new WorkerView();
+        //             workerwelcome.Show();
+        //             break;
+        //         case 2:
+        //             WorkerView leaderwelcome = new WorkerView();
+        //             leaderwelcome.Show();
+        //             break;
+        //         case 3:
+        //             HeadView headwelcome = new HeadView();
+        //             headwelcome.Show();
+        //             break;
+        //         case 4:
+        //             HeadView adminwelcome = new HeadView();
+        //             adminwelcome.Show();
+        //             break;
+        //
+        //     }
+        //     this.Close();
+        //
+        // }
+        //
         private void MainGrid_Loaded(object sender, RoutedEventArgs e)
         {
             var dialog = new LoginPrompt();
             var client = GlobalUsage.GetRtClient();
             dialog.Closing += (_sender, _e) =>
             {
+
                 var d = _sender as LoginPrompt;
                 if (!(d.Canceled))
                 {
 
                     var id = Convert.ToInt32(d.InputTextBox.Text);
                     var password = d.PasswrdInput.Password;
+
                     /*var response = client.LogIn(new LoginRequest {Id = id, Password = password});
                     if (response.State)
                     {
